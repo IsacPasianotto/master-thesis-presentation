e905af84689ac24f2b78ad31f3a4fdbdc08ab43d
IsacPasianotto <yh4mop12z@gmail.com>
2024-09-12 15:55:30 +0200

    removed wrongly uploaded files

--- defense/auto/title-slide.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(TeX-add-style-hook
- "title-slide"
- (lambda ()
-   (LaTeX-add-environments
-    '("quote" LaTeX-env-args ["argument"] 1)))
- :latex)
-
--- resources/auto/aesthetics.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(TeX-add-style-hook
- "aesthetics"
- (lambda ()
-   (LaTeX-add-environments
-    '("quote" LaTeX-env-args ["argument"] 1)))
- :plain-tex)
-
