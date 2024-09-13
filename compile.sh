b2573fa937240ff17f13a2b120f0150cb0cd3d07
IsacPasianotto <yh4mop12z@gmail.com>
2024-09-13 18:50:42 +0200

    first draft of the first frames

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo -e "Error: Invalid number of arguments!\nPlease provide the filename as an argument:\n\t./compile.sh <filename>"
+  exit 1
+fi
+
+targetfile=$1
+
+if [ ! -f $targetfile ]; then
+    echo "Target file $targetfile not found!"
+    exit 1
+fi
+
+# compile the target file
+lualatex -shell-escape $targetfile
+# recompile it for the references
+lualatex -shell-escape $targetfile
